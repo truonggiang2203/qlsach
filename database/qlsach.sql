1a73c4280f8b4efd7fdf8082cef7362b2fec0342
LioraNhat <tramtranyeah2@gmail.com>
2025-10-24 14:01:28 +0700

    .

--- database/qlsach.sql
+++ database/qlsach.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th10 10, 2025 lúc 09:18 AM
--- Phiên bản máy phục vụ: 10.4.32-MariaDB
--- Phiên bản PHP: 8.1.25
+-- Host: 127.0.0.1
+-- Generation Time: Oct 24, 2025 at 09:00 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Cơ sở dữ liệu: `qlsach`
+-- Database: `qlsach`
 --
 
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `binh_luan`
+-- Table structure for table `binh_luan`
 --
 
 CREATE TABLE `binh_luan` (
@@ -37,7 +37,7 @@ CREATE TABLE `binh_luan` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `chi_tiet_don_hang`
+-- Table structure for table `chi_tiet_don_hang`
 --
 
 CREATE TABLE `chi_tiet_don_hang` (
@@ -49,7 +49,7 @@ CREATE TABLE `chi_tiet_don_hang` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `chi_tiet_phieu_nhap`
+-- Table structure for table `chi_tiet_phieu_nhap`
 --
 
 CREATE TABLE `chi_tiet_phieu_nhap` (
@@ -62,7 +62,7 @@ CREATE TABLE `chi_tiet_phieu_nhap` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `don_hang`
+-- Table structure for table `don_hang`
 --
 
 CREATE TABLE `don_hang` (
@@ -76,7 +76,7 @@ CREATE TABLE `don_hang` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `gia_sach`
+-- Table structure for table `gia_sach`
 --
 
 CREATE TABLE `gia_sach` (
@@ -88,7 +88,7 @@ CREATE TABLE `gia_sach` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `khuyen_mai`
+-- Table structure for table `khuyen_mai`
 --
 
 CREATE TABLE `khuyen_mai` (
@@ -103,7 +103,7 @@ CREATE TABLE `khuyen_mai` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `loai_sach`
+-- Table structure for table `loai_sach`
 --
 
 CREATE TABLE `loai_sach` (
@@ -114,7 +114,7 @@ CREATE TABLE `loai_sach` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `ngon_ngu`
+-- Table structure for table `ngon_ngu`
 --
 
 CREATE TABLE `ngon_ngu` (
@@ -125,7 +125,7 @@ CREATE TABLE `ngon_ngu` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `nguoi_dung`
+-- Table structure for table `nguoi_dung`
 --
 
 CREATE TABLE `nguoi_dung` (
@@ -133,10 +133,18 @@ CREATE TABLE `nguoi_dung` (
   `phan_quyen` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `nguoi_dung`
+--
+
+INSERT INTO `nguoi_dung` (`id_nd`, `phan_quyen`) VALUES
+('AD', 'admin'),
+('KH', 'khach_hang');
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `nha_cung_cap`
+-- Table structure for table `nha_cung_cap`
 --
 
 CREATE TABLE `nha_cung_cap` (
@@ -151,7 +159,7 @@ CREATE TABLE `nha_cung_cap` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `nxb`
+-- Table structure for table `nxb`
 --
 
 CREATE TABLE `nxb` (
@@ -162,7 +170,7 @@ CREATE TABLE `nxb` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `phieu_nhap`
+-- Table structure for table `phieu_nhap`
 --
 
 CREATE TABLE `phieu_nhap` (
@@ -177,7 +185,7 @@ CREATE TABLE `phieu_nhap` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `phuong_thuc_thanh_toan`
+-- Table structure for table `phuong_thuc_thanh_toan`
 --
 
 CREATE TABLE `phuong_thuc_thanh_toan` (
@@ -188,7 +196,7 @@ CREATE TABLE `phuong_thuc_thanh_toan` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `sach`
+-- Table structure for table `sach`
 --
 
 CREATE TABLE `sach` (
@@ -205,7 +213,7 @@ CREATE TABLE `sach` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `s_ncc`
+-- Table structure for table `s_ncc`
 --
 
 CREATE TABLE `s_ncc` (
@@ -216,7 +224,7 @@ CREATE TABLE `s_ncc` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `s_nns`
+-- Table structure for table `s_nns`
 --
 
 CREATE TABLE `s_nns` (
@@ -227,7 +235,7 @@ CREATE TABLE `s_nns` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `s_tg`
+-- Table structure for table `s_tg`
 --
 
 CREATE TABLE `s_tg` (
@@ -238,7 +246,7 @@ CREATE TABLE `s_tg` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tac_gia`
+-- Table structure for table `tac_gia`
 --
 
 CREATE TABLE `tac_gia` (
@@ -249,7 +257,7 @@ CREATE TABLE `tac_gia` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `tai_khoan`
+-- Table structure for table `tai_khoan`
 --
 
 CREATE TABLE `tai_khoan` (
@@ -264,10 +272,22 @@ CREATE TABLE `tai_khoan` (
   `ngay_gio_tao_tk` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `tai_khoan`
+--
+
+INSERT INTO `tai_khoan` (`id_tk`, `id_nd`, `ho_ten`, `gioi_tinh`, `email`, `sdt`, `mat_khau`, `dia_chi_giao_hang`, `ngay_gio_tao_tk`) VALUES
+('AD001', 'AD', 'Tram123', 'Khác', 'admin@qlsach.com', '0000000001', '$2y$10$TfuNpwml2a5EmP8G1uBpPO5VQtpJ.OnRxvs3QvuldOAeaQFHPjg4S', '123 Server, TP. HCM', '2025-10-20 21:51:53'),
+('AD002', 'AD', 'Thuong123', 'Khác', 'admin_kho@qlsach.com', '0000000002', '$2y$10$TfuNpwml2a5EmP8G1uBpPO5VQtpJ.OnRxvs3QvuldOAeaQFHPjg4S', '123 Server, TP. HCM', '2025-10-20 21:51:53'),
+('AD003', 'AD', 'Giang123', 'Khác', 'admin_content@qlsach.com', '0000000003', '$2y$10$TfuNpwml2a5EmP8G1uBpPO5VQtpJ.OnRxvs3QvuldOAeaQFHPjg4S', '123 Server, TP. HCM', '2025-10-20 21:51:53'),
+('KH001', 'KH', 'Nguyễn Văn A', 'Nam', 'khach1@gmail.com', '0901000111', '$2y$10$f4e.9UWG5FwITxwjLnG8uO7HLlgA4VkfYBlTwzW9hpldIFACj.QGO', '456 Đường ABC, Q1, TP. HCM', '2025-10-20 21:51:53'),
+('KH002', 'KH', 'Trần Thị B', 'Nữ', 'khach2@gmail.com', '0902000222', '$2y$10$f4e.9UWG5FwITxwjLnG8uO7HLlgA4VkfYBlTwzW9hpldIFACj.QGO', '789 Đường XYZ, Q3, TP. HCM', '2025-10-20 21:51:53'),
+('TK221', 'KH', 'tran ngoc lam', 'Khác', 'lam@gmail.com', '012345', '$2y$10$Z8LWwS9pmbjBZtu80eeyeeekG0lGqYHwzpXr4hu6o9gtoaE5K2wUG', 'Cần Thơ', '2025-10-20 21:58:04');
+
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `thanh_toan`
+-- Table structure for table `thanh_toan`
 --
 
 CREATE TABLE `thanh_toan` (
@@ -280,7 +300,7 @@ CREATE TABLE `thanh_toan` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `the_loai`
+-- Table structure for table `the_loai`
 --
 
 CREATE TABLE `the_loai` (
@@ -292,7 +312,7 @@ CREATE TABLE `the_loai` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `thoi_diem`
+-- Table structure for table `thoi_diem`
 --
 
 CREATE TABLE `thoi_diem` (
@@ -302,7 +322,7 @@ CREATE TABLE `thoi_diem` (
 -- --------------------------------------------------------
 
 --
--- Cấu trúc bảng cho bảng `trang_thai_don_hang`
+-- Table structure for table `trang_thai_don_hang`
 --
 
 CREATE TABLE `trang_thai_don_hang` (
@@ -311,32 +331,32 @@ CREATE TABLE `trang_thai_don_hang` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Chỉ mục cho các bảng đã đổ
+-- Indexes for dumped tables
 --
 
 --
--- Chỉ mục cho bảng `binh_luan`
+-- Indexes for table `binh_luan`
 --
 ALTER TABLE `binh_luan`
   ADD PRIMARY KEY (`id_bl`),
   ADD KEY `id_sach` (`id_sach`);
 
 --
--- Chỉ mục cho bảng `chi_tiet_don_hang`
+-- Indexes for table `chi_tiet_don_hang`
 --
 ALTER TABLE `chi_tiet_don_hang`
   ADD PRIMARY KEY (`id_don_hang`,`id_sach`),
   ADD KEY `id_sach` (`id_sach`);
 
 --
--- Chỉ mục cho bảng `chi_tiet_phieu_nhap`
+-- Indexes for table `chi_tiet_phieu_nhap`
 --
 ALTER TABLE `chi_tiet_phieu_nhap`
   ADD PRIMARY KEY (`id_phieu_nhap`,`id_sach`),
   ADD KEY `id_sach` (`id_sach`);
 
 --
--- Chỉ mục cho bảng `don_hang`
+-- Indexes for table `don_hang`
 --
 ALTER TABLE `don_hang`
   ADD PRIMARY KEY (`id_don_hang`),
@@ -344,63 +364,63 @@ ALTER TABLE `don_hang`
   ADD KEY `id_trang_thai` (`id_trang_thai`);
 
 --
--- Chỉ mục cho bảng `gia_sach`
+-- Indexes for table `gia_sach`
 --
 ALTER TABLE `gia_sach`
   ADD PRIMARY KEY (`id_sach`,`ngay_gio_ban`),
   ADD KEY `ngay_gio_ban` (`ngay_gio_ban`);
 
 --
--- Chỉ mục cho bảng `khuyen_mai`
+-- Indexes for table `khuyen_mai`
 --
 ALTER TABLE `khuyen_mai`
   ADD PRIMARY KEY (`id_km`);
 
 --
--- Chỉ mục cho bảng `loai_sach`
+-- Indexes for table `loai_sach`
 --
 ALTER TABLE `loai_sach`
   ADD PRIMARY KEY (`id_loai`);
 
 --
--- Chỉ mục cho bảng `ngon_ngu`
+-- Indexes for table `ngon_ngu`
 --
 ALTER TABLE `ngon_ngu`
   ADD PRIMARY KEY (`id_ngon_ngu`);
 
 --
--- Chỉ mục cho bảng `nguoi_dung`
+-- Indexes for table `nguoi_dung`
 --
 ALTER TABLE `nguoi_dung`
   ADD PRIMARY KEY (`id_nd`);
 
 --
--- Chỉ mục cho bảng `nha_cung_cap`
+-- Indexes for table `nha_cung_cap`
 --
 ALTER TABLE `nha_cung_cap`
   ADD PRIMARY KEY (`id_ncc`);
 
 --
--- Chỉ mục cho bảng `nxb`
+-- Indexes for table `nxb`
 --
 ALTER TABLE `nxb`
   ADD PRIMARY KEY (`id_nxb`);
 
 --
--- Chỉ mục cho bảng `phieu_nhap`
+-- Indexes for table `phieu_nhap`
 --
 ALTER TABLE `phieu_nhap`
   ADD PRIMARY KEY (`id_phieu_nhap`),
   ADD KEY `id_ncc` (`id_ncc`);
 
 --
--- Chỉ mục cho bảng `phuong_thuc_thanh_toan`
+-- Indexes for table `phuong_thuc_thanh_toan`
 --
 ALTER TABLE `phuong_thuc_thanh_toan`
   ADD PRIMARY KEY (`id_pttt`);
 
 --
--- Chỉ mục cho bảng `sach`
+-- Indexes for table `sach`
 --
 ALTER TABLE `sach`
   ADD PRIMARY KEY (`id_sach`),
@@ -409,121 +429,121 @@ ALTER TABLE `sach`
   ADD KEY `id_km` (`id_km`);
 
 --
--- Chỉ mục cho bảng `s_ncc`
+-- Indexes for table `s_ncc`
 --
 ALTER TABLE `s_ncc`
   ADD PRIMARY KEY (`id_sach`,`id_ncc`),
   ADD KEY `id_ncc` (`id_ncc`);
 
 --
--- Chỉ mục cho bảng `s_nns`
+-- Indexes for table `s_nns`
 --
 ALTER TABLE `s_nns`
   ADD PRIMARY KEY (`id_sach`,`id_ngon_ngu`),
   ADD KEY `id_ngon_ngu` (`id_ngon_ngu`);
 
 --
--- Chỉ mục cho bảng `s_tg`
+-- Indexes for table `s_tg`
 --
 ALTER TABLE `s_tg`
   ADD PRIMARY KEY (`id_sach`,`id_tac_gia`),
   ADD KEY `id_tac_gia` (`id_tac_gia`);
 
 --
--- Chỉ mục cho bảng `tac_gia`
+-- Indexes for table `tac_gia`
 --
 ALTER TABLE `tac_gia`
   ADD PRIMARY KEY (`id_tac_gia`);
 
 --
--- Chỉ mục cho bảng `tai_khoan`
+-- Indexes for table `tai_khoan`
 --
 ALTER TABLE `tai_khoan`
   ADD PRIMARY KEY (`id_tk`),
   ADD KEY `id_nd` (`id_nd`);
 
 --
--- Chỉ mục cho bảng `thanh_toan`
+-- Indexes for table `thanh_toan`
 --
 ALTER TABLE `thanh_toan`
   ADD PRIMARY KEY (`id_pttt`,`id_don_hang`),
   ADD KEY `id_don_hang` (`id_don_hang`);
 
 --
--- Chỉ mục cho bảng `the_loai`
+-- Indexes for table `the_loai`
 --
 ALTER TABLE `the_loai`
   ADD PRIMARY KEY (`id_the_loai`),
   ADD KEY `id_loai` (`id_loai`);
 
 --
--- Chỉ mục cho bảng `thoi_diem`
+-- Indexes for table `thoi_diem`
 --
 ALTER TABLE `thoi_diem`
   ADD PRIMARY KEY (`ngay_gio_ban`);
 
 --
--- Chỉ mục cho bảng `trang_thai_don_hang`
+-- Indexes for table `trang_thai_don_hang`
 --
 ALTER TABLE `trang_thai_don_hang`
   ADD PRIMARY KEY (`id_trang_thai`);
 
 --
--- AUTO_INCREMENT cho các bảng đã đổ
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT cho bảng `trang_thai_don_hang`
+-- AUTO_INCREMENT for table `trang_thai_don_hang`
 --
 ALTER TABLE `trang_thai_don_hang`
   MODIFY `id_trang_thai` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Các ràng buộc cho các bảng đã đổ
+-- Constraints for dumped tables
 --
 
 --
--- Các ràng buộc cho bảng `binh_luan`
+-- Constraints for table `binh_luan`
 --
 ALTER TABLE `binh_luan`
   ADD CONSTRAINT `binh_luan_ibfk_1` FOREIGN KEY (`id_sach`) REFERENCES `sach` (`id_sach`);
 
 --
--- Các ràng buộc cho bảng `chi_tiet_don_hang`
+-- Constraints for table `chi_tiet_don_hang`
 --
 ALTER TABLE `chi_tiet_don_hang`
   ADD CONSTRAINT `chi_tiet_don_hang_ibfk_1` FOREIGN KEY (`id_don_hang`) REFERENCES `don_hang` (`id_don_hang`),
   ADD CONSTRAINT `chi_tiet_don_hang_ibfk_2` FOREIGN KEY (`id_sach`) REFERENCES `sach` (`id_sach`);
 
 --
--- Các ràng buộc cho bảng `chi_tiet_phieu_nhap`
+-- Constraints for table `chi_tiet_phieu_nhap`
 --
 ALTER TABLE `chi_tiet_phieu_nhap`
   ADD CONSTRAINT `chi_tiet_phieu_nhap_ibfk_1` FOREIGN KEY (`id_phieu_nhap`) REFERENCES `phieu_nhap` (`id_phieu_nhap`),
   ADD CONSTRAINT `chi_tiet_phieu_nhap_ibfk_2` FOREIGN KEY (`id_sach`) REFERENCES `sach` (`id_sach`);
 
 --
--- Các ràng buộc cho bảng `don_hang`
+-- Constraints for table `don_hang`
 --
 ALTER TABLE `don_hang`
   ADD CONSTRAINT `don_hang_ibfk_1` FOREIGN KEY (`id_tk`) REFERENCES `tai_khoan` (`id_tk`),
   ADD CONSTRAINT `don_hang_ibfk_2` FOREIGN KEY (`id_trang_thai`) REFERENCES `trang_thai_don_hang` (`id_trang_thai`);
 
 --
--- Các ràng buộc cho bảng `gia_sach`
+-- Constraints for table `gia_sach`
 --
 ALTER TABLE `gia_sach`
   ADD CONSTRAINT `gia_sach_ibfk_1` FOREIGN KEY (`id_sach`) REFERENCES `sach` (`id_sach`),
   ADD CONSTRAINT `gia_sach_ibfk_2` FOREIGN KEY (`ngay_gio_ban`) REFERENCES `thoi_diem` (`ngay_gio_ban`);
 
 --
--- Các ràng buộc cho bảng `phieu_nhap`
+-- Constraints for table `phieu_nhap`
 --
 ALTER TABLE `phieu_nhap`
   ADD CONSTRAINT `phieu_nhap_ibfk_1` FOREIGN KEY (`id_ncc`) REFERENCES `nha_cung_cap` (`id_ncc`);
 
 --
--- Các ràng buộc cho bảng `sach`
+-- Constraints for table `sach`
 --
 ALTER TABLE `sach`
   ADD CONSTRAINT `sach_ibfk_1` FOREIGN KEY (`id_loai`) REFERENCES `loai_sach` (`id_loai`),
@@ -531,41 +551,41 @@ ALTER TABLE `sach`
   ADD CONSTRAINT `sach_ibfk_3` FOREIGN KEY (`id_km`) REFERENCES `khuyen_mai` (`id_km`);
 
 --
--- Các ràng buộc cho bảng `s_ncc`
+-- Constraints for table `s_ncc`
 --
 ALTER TABLE `s_ncc`
   ADD CONSTRAINT `s_ncc_ibfk_1` FOREIGN KEY (`id_sach`) REFERENCES `sach` (`id_sach`),
   ADD CONSTRAINT `s_ncc_ibfk_2` FOREIGN KEY (`id_ncc`) REFERENCES `nha_cung_cap` (`id_ncc`);
 
 --
--- Các ràng buộc cho bảng `s_nns`
+-- Constraints for table `s_nns`
 --
 ALTER TABLE `s_nns`
   ADD CONSTRAINT `s_nns_ibfk_1` FOREIGN KEY (`id_sach`) REFERENCES `sach` (`id_sach`),
   ADD CONSTRAINT `s_nns_ibfk_2` FOREIGN KEY (`id_ngon_ngu`) REFERENCES `ngon_ngu` (`id_ngon_ngu`);
 
 --
--- Các ràng buộc cho bảng `s_tg`
+-- Constraints for table `s_tg`
 --
 ALTER TABLE `s_tg`
   ADD CONSTRAINT `s_tg_ibfk_1` FOREIGN KEY (`id_sach`) REFERENCES `sach` (`id_sach`),
   ADD CONSTRAINT `s_tg_ibfk_2` FOREIGN KEY (`id_tac_gia`) REFERENCES `tac_gia` (`id_tac_gia`);
 
 --
--- Các ràng buộc cho bảng `tai_khoan`
+-- Constraints for table `tai_khoan`
 --
 ALTER TABLE `tai_khoan`
   ADD CONSTRAINT `tai_khoan_ibfk_1` FOREIGN KEY (`id_nd`) REFERENCES `nguoi_dung` (`id_nd`);
 
 --
--- Các ràng buộc cho bảng `thanh_toan`
+-- Constraints for table `thanh_toan`
 --
 ALTER TABLE `thanh_toan`
   ADD CONSTRAINT `thanh_toan_ibfk_1` FOREIGN KEY (`id_pttt`) REFERENCES `phuong_thuc_thanh_toan` (`id_pttt`),
   ADD CONSTRAINT `thanh_toan_ibfk_2` FOREIGN KEY (`id_don_hang`) REFERENCES `don_hang` (`id_don_hang`);
 
 --
--- Các ràng buộc cho bảng `the_loai`
+-- Constraints for table `the_loai`
 --
 ALTER TABLE `the_loai`
   ADD CONSTRAINT `the_loai_ibfk_1` FOREIGN KEY (`id_loai`) REFERENCES `loai_sach` (`id_loai`);
